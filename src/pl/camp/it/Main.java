4f021aa95069b7951e4f45c474a78cdf67460110
Mateusz <mat.bereda@gmail.com>
2021-04-10 11:44:07 +0200

    Komentarz

--- src/pl/camp/it/Main.java
+++ src/pl/camp/it/Main.java
@@ -5,5 +5,7 @@ public class Main {
         System.out.println("Hello2 !!");
         System.out.println("hello");
         System.out.println("hello2");
+        System.out.println("asdfgas");
+        System.out.println("asdgfasfg");
     }
 }
