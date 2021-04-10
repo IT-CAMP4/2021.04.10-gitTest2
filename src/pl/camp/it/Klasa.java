fdad39dbd4833c1088ac2bfb1db8cc7802e2e818
Mateusz <mat.bereda@gmail.com>
2021-04-10 11:50:25 +0200

    cos

--- src/pl/camp/it/Klasa.java
+++ src/pl/camp/it/Klasa.java
@@ -2,6 +2,7 @@ package pl.camp.it;
 
 public class Klasa {
     private int id;
+    private int a;
 
     public int getId() {
         return id;
@@ -9,5 +10,13 @@ public class Klasa {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
     }
 }
