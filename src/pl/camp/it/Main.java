0133123a5ae0798f8cc73a41923fb94aeaaf15cc
Mateusz <mat.bereda@gmail.com>
2021-04-10 11:40:50 +0200

    asdfasdf

--- src/pl/camp/it/Main.java
+++ src/pl/camp/it/Main.java
@@ -3,5 +3,7 @@ package pl.camp.it;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello2 !!");
+        System.out.println("hello");
+        System.out.println("hello2");
     }
 }
